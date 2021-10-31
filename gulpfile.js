55cc6da8f293a6d15efc210a5b8ce4947e616d60
Beth <Beth3346@gmail.com>
2021-10-30 22:08:51 -0500

    update dependencies and file structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,90 +1,55 @@
-const gulp = require('gulp');
-const path = require('path');
-const sass = require('gulp-sass');
-const plumber = require('gulp-plumber');
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
-const browsersync = require('browser-sync').create();
-const del = require('del');
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+const del = require("del");
 
 var paths = {
-  app: 'dist/',
-  css: 'dist/css/',
-  images: 'dist/images/',
-  scss: './*.scss',
-  html: 'dist/*.html',
-  srcImages: [
-    'src/images/*.jpg',
-    'src/images/*.png',
-    'src/images/*.jpeg',
-    'src/images/*.gif'
-  ]
+  app: "dist/",
+  css: "dist/css/",
+  images: "dist/images/",
 };
-
-// BrowserSync
-function browserSync(done) {
-  browsersync.init({
-    server: {
-      baseDir: './dist/'
-    },
-    port: 3000
-  });
-  done();
-}
-
-// BrowserSync Reload
-function browserSyncReload(done) {
-  browsersync.reload();
-  done();
-}
 
 // Clean assets
 function clean() {
-  return del([paths.css, paths.images]);
+  return del(["./dist"]);
 }
 
-// Watch files
-function watchFiles(done) {
-  gulp.watch(['./src', paths.html, './*.scss'], () => {
-    gulp.src(paths.srcImages).pipe(gulp.dest(paths.images));
-
-    return gulp
-      .src(paths.scss)
-      .pipe(plumber())
-      .pipe(
-        sass({
-          includePaths: ['node_modules']
-        }).on('error', sass.logError)
-      )
-      .pipe(gulp.dest(paths.css))
-      .pipe(browserSyncReload(done));
-  });
+// Copy HTML
+function copyIndex() {
+  return gulp.src(["./public/*.html"]).pipe(gulp.dest(paths.app));
 }
 
-gulp.task('default', () => {
-  gulp
-    .src(paths.scss)
+function copyImages() {
+  return gulp
+    .src("public/images/**/*.{gif,jpg,png,svg}")
+    .pipe(gulp.dest(paths.images));
+}
+
+gulp.task("default", () => {
+  copyImages();
+
+  return gulp
+    .src(["./public/*.scss"])
     .pipe(plumber())
     .pipe(
       sass({
-        // includePaths: ['node_modules']
-      }).on('error', sass.logError)
+        includePaths: ["node_modules"],
+      }).on("error", sass.logError)
     )
     .pipe(gulp.dest(paths.css))
     .pipe(
       autoprefixer({
-        overrideBrowserslist: ['last 3 versions'],
-        cascade: false
+        cascade: false,
       })
     )
     .pipe(gulp.dest(paths.css))
     .pipe(cleanCSS({ debug: true }))
-    .pipe(gulp.dest(paths.css));
-
-  return gulp.src(paths.srcImages).pipe(gulp.dest(paths.images));
+    .pipe(gulp.dest(paths.css))
+    .pipe(copyIndex());
 });
 
-const watch = gulp.parallel(watchFiles, browserSync);
-
 exports.clean = clean;
-exports.watch = watch;
+exports.copyIndex = copyIndex;
+exports.copyImages = copyImages;
